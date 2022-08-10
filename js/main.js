8da71c9c556cfa61476fd6c5d184a2fb08855264
rachelschipull <rlschipull@gmail.com>
2022-08-10 15:31:19 -0400

    update comments

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@
 document.querySelector('.button-25').addEventListener('click', getMorse)
 
 async function getMorse(){
-
 //grab the value = (number) from the form
 const number = document.querySelector('input').value
 //pull number random elements from the object 
@@ -53,27 +52,32 @@ const dictionary = {
   "(": "-.--.*",
   ")": "-.--.-*"
 }
-
+//set constant to target individual keys from the dicitionary
 const keys = Object.keys(dictionary)
-//const randoms = keys[Math.floor(Math.random() * keys.length)]
 //console.log(randoms)
-const playArray = []
 
+//set array to hold keys
+const playArray = []
+//loop to select random keys from the dictionary and place into array
 for ( let i = 1; i <= number; i++){
   const randoms = keys[Math.floor(Math.random() * keys.length)]
   playArray.push(randoms)
 }
 //console.log(playArray)
 
+//maps the code into the array vs the key names
 const dashAndDots = playArray.map( sign => dictionary[sign] )
 //console.log(dashAndDots)
-//code to play them in the DOM
+
+//constants to add sounds to the Audio API
 const dashSound = new Audio('dash.mp3')
 //dashSound.play()
 const dotSound = new Audio('dot.mp3')
 //dotSound.play()
 const blankSound = new Audio('blank.mp3')
-//add spaces between elements in the array (when building array or during the playing process)
+//adds spaces between elements in the array (when building array or during the playing process)
+
+//nested loop to play over the dashes and dots and add space between them to make them audible
 for (let i = 0; i < dashAndDots.length; i++){
   //console.log(dashAndDots[i])
   for (let j = 0; j < dashAndDots[i].length; j++){
@@ -82,15 +86,19 @@ for (let i = 0; i < dashAndDots.length; i++){
     console.log(dashAndDots[i][j])
     if (dashAndDots[i][j] === '.'){ 
 
+      //plays dot sounds
       function playAudio(dotSound){
        return new Promise(res=>{
           dotSound.play()
+          //on the end of the dot sound adds 200 ms of time before the await
           dotSound.onended = function spaces() {
             setTimeout(() => res(), 200)}
         })
       }
+      //once the function has completed, moves on to next j index
       await playAudio(dotSound)
       //dotSound.play()
+
       } else if (dashAndDots[i][j] === '-'){ 
         function playAudio(dashSound){
         return new Promise(res=>{
@@ -101,6 +109,7 @@ for (let i = 0; i < dashAndDots.length; i++){
           }
       await playAudio(dashSound)
       //dashSound.play()}
+
       } else {
         function playAudio(blankSound){
           return new Promise( res => {
@@ -113,11 +122,12 @@ for (let i = 0; i < dashAndDots.length; i++){
       }
     }
   }
-//print in the DOM, but not be visible 
+//makes the playArray visible in the DOM
 document.querySelector('#solutionId').addEventListener('click', revealMorse)
 
 function revealMorse(){
   console.log(playArray)
+  //prints the playArray to the h3 in the DOM
   document.querySelector('h3').innerText = `${playArray.join(' ')}`
 }
 }
